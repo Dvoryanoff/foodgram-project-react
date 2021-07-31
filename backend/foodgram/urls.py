205e5801aae4b113f19bcfb366fe07a49967945f
Dvoryanoff <dvoryanoff@mail.ru>
2021-07-31 14:12:48 +0300

    Поменял структуру проекта.

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,4 +1,4 @@
-"""foodgram URL Configuration
+"""foodgramhu URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-# from api import urls
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/v1/', include('api.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.jwt')),
-
 ]
--- backend/foodgram/wsgi.py
+++ backend/foodgram/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for foodgram project.
+WSGI config for foodgramhu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgramhu.settings')
 
 application = get_wsgi_application()
