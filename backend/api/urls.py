f08026b0b77297f9f7c7dd77424aa995c923362e
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-10 12:01:07 +0300

    Routing, urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import SubscribeListViewSet, SubscribeView, UsersViewSet
+from .views import (FavoriteViewSet, IngredientViewSet, RecipeViewSet,
+                    ShoppingCartViewSet, SubscribeListViewSet,
+                    SubscribeView, TagViewSet, UsersViewSet)
+
+v1_router = DefaultRouter()
+v1_router.register('users', UsersViewSet)
+v1_router.register('recipes', RecipeViewSet, basename='recipes')
+v1_router.register('ingredients', IngredientViewSet, basename='ingredients')
+v1_router.register('tags', TagViewSet, basename='tag')
 
 urlpatterns = [
     path(
@@ -11,11 +19,20 @@ urlpatterns = [
         ),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include('djoser.urls.jwt')),
-
+    path(
+        'recipes/<int:recipe_id>/shopping_cart/',
+        ShoppingCartViewSet.as_view(),
+        name='shopping_cart'
+        ),
+    path(
+        'recipes/<int:recipe_id>/favorite/',
+        FavoriteViewSet.as_view(),
+        name='favorite'
+        ),
     path(
         'users/<int:user_id>/subscribe/',
         SubscribeView.as_view(),
         name='follow'
         ),
+    path('', include(v1_router.urls)),
 ]
