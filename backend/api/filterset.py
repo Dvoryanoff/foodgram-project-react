e9e3425b32cc6493bb0aa4a6ec6dad80a76a8981
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-18 20:06:13 +0300

    local conteiners-start, Subscribe and Follow fix, +DockerHub+Flake8

--- backend/api/filterset.py
+++ backend/api/filterset.py
@@ -4,7 +4,8 @@ from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 
 
 class IngredientFilter(django_filters.rest_framework.FilterSet):
-    name = django_filters.CharFilter(field_name='name', lookup_expr='istartswith')
+    name = django_filters.CharFilter(field_name='name',
+                                     lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
