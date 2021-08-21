d14a37b5a424a3976118fc15ab528493a6deca77
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-21 13:06:08 +0300

    web_static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -139,7 +139,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "web_static")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "web_media")
