2da0deedcdc1d3284b2dc81a7d6eeea91fd7ee23
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-22 15:12:02 +0300

    РАБОТАЕТ ЛОКАЛЬНО В КОНТЕЙНЕРАХ СО СБОРКОЙ С DOCKER.HUB_page_size_3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 3,
 }
 
 DJOSER = {
