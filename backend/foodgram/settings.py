836ae6ba3721e45319f8d5674286c42b9d418c61
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-23 17:07:49 +0300

    CustomPagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -106,9 +106,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+    'PAGE_SIZE': 3,
 }
 
 DJOSER = {
