a6883f9a59a06f34d4b769cbfb1c47d7360135dd
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-23 14:36:00 +0300

    что-то сломано

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
     'import_export',
