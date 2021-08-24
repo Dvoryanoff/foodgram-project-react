e4fa3f5c502bb83c1f15d5f6715c9b88425f5cae
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-24 14:53:45 +0300

    Fix same ingredients 3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
+from rest_framework import serializers
 
 from recipes.models import (Favorite, Follow, Ingredient, IngredientAmount,
                             Recipe, ShoppingCart, Tag, TagRecipe)
