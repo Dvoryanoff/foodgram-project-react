05f12ee3cf264c6e4f41c4871c92c8ff73f13784
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-25 14:08:18 +0300

    удвлил page параметр из paginators

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -1,8 +1,5 @@
-from rest_framework import pagination
+from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPagination(pagination.PageNumberPagination):
-
-    page_size = 10
-    page_query_param = "page"
-    page_size_query_param = "limit"
+class PageNumberPaginatorModified(PageNumberPagination):
+    page_size_query_param = 'limit'
