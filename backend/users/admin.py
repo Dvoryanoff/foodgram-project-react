8db652ede6e3556ce1adc4f5ddffcad5a7a8b7fc
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-25 13:56:56 +0300

    users_admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
 
-from users.models import CustomUser
+from .models import User
 
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'username',
-        'email',
-    )
-    search_fields = (
-        'username',
-        'email')
-    list_filter = (
-        'username',
-        'email')
-
-
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(User)
