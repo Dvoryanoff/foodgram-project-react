2a680d7b90f48d259cedf25619c17f20593873e9
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-25 13:50:06 +0300

    asgi.py, foodgram.urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/', include('recipes.urls')),
+    path('api/', include('users.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
 ]
