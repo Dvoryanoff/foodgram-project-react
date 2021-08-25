7240cb5b97cd74e530371288164c40584304cf43
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-25 13:51:26 +0300

    abstract_user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,31 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .managers import CustomUserManager
 
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(
-        unique=True,
-        verbose_name='email address')
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-        verbose_name='user name')
-    first_name = models.CharField(
-        max_length=150,
-        verbose_name='first name')
-    last_name = models.CharField(
-        max_length=150,
-        verbose_name='last name')
-
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=254)
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
-
-    objects = CustomUserManager()
 
     class Meta:
-        ordering = ['-id']
+        ordering = ('username', )
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.username
