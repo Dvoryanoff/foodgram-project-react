5986e3ad6045ae4f0eea6d3c29bc4797a1fd051e
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-26 19:02:56 +0300

    Пробуем WorkFlow2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = 'lu@6n%b25_v)350ax_pyd)!l6ia&&e@1i+t*zczh7h7mry$yg2'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'testserver', 'web', '130.193.38.100', 'localhost', 'www.dvoryanoff.tk', 'dvoryanoff.tk']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1', 'testserver', 'web', '130.193.38.100', 'localhost', 'www.dvoryanoff.tk', 'dvoryanoff.tk']
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
 
