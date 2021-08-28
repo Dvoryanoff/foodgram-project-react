9b497fc51abdbe818c3bc5fd88515c870cb8268d
Dvoryanoff <dvoryanoff@mail.ru>
2021-08-28 10:37:54 +0300

    cooking_time10

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
+from django.core import validators
 from django.db import models
 
 User = get_user_model()
